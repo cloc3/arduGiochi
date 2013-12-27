d12d5d49708279da52f5923fc1ab352ffa8a9259
cloc3@s939 <ziapannocchia@gmail.com>
2013-12-27 16:57:45 +0100

    studio per acquisizione una stringa numerica dalla porta seriale

--- accelerometro/req.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-	timestamp=$(date +%s)
-	echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nAccess-Control-Allow-Origin: *\r\nKeep-Alive: timeout=2, max=100\r\nConnection: Keep-Alive\r\nTimestamp: ${timestamp}\r\n\r\n"
