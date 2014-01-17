a57946155e221caa33a1904377734e639f63d065
cloc3@s939 <ziapannocchia@gmail.com>
2014-01-17 12:58:02 +0100

    pulizia del codice javascritp e inseriemento commenti

--- adxlWeb/js/scripts.js
+++ adxlWeb/js/scripts.js
@@ -35,55 +35,58 @@ function putContent(content,elementId) {
     e.innerHTML = content;
 }
 
-function gestioneGrafici() {
+function gestioneGrafici(velox) {
 
+// preparazione del vettore globale che contiene i dati da rappresentare nella canvas 
 	eventi = new Array;
-	traslazione = 0;
+
 	xCanvas = document.getElementById('graficoX');
 	yCanvas = document.getElementById('graficoY');
 	zCanvas = document.getElementById('graficoZ');
 	ctx = xCanvas.getContext('2d');
 	cty = yCanvas.getContext('2d');
 	ctz = zCanvas.getContext('2d');
-	ctx.fillStyle = "rgb(200,0,0)";
-	cty.fillStyle = "rgb(0,200,0)";
-	ctz.fillStyle = "rgb(0,0,200)";
-	ajaxGet("http://localhost:8000",graph,"footer",0);
+
+	ajaxGet("http://localhost:8000",graph,0,velox);
 }
 
 // routine di gestione della canvas
 
-function graph(content,elementId,tempoFinale) {
-	var e = document.getElementById(elementId)
-	var e = document.getElementById("footer")
-	var nuoviEventi = content.split("\n");
+function graph(content,tempoFinale,velox) {
+
 	var tempoFinalePrecedente = tempoFinale;
+
+// acquisizione dei dati e aggiornamento del vettore eventi.
+	var nuoviEventi = content.split("\n");
 	nuoviEventi.pop();
 	var tempoFinale = (nuoviEventi[nuoviEventi.length-1].split(" "))[0]
 	eventi=nuoviEventi.concat(eventi);
-	var n;
-	if ( n = eventi.length ) {
-		while ((tempoFinale - eventi[n=n-1].split(" ")[0])/50000 >600) eventi.pop()
-	}
 
+// rimozione dal vettore eventi dei dati in eccesso
+// (troppo lontani nel passato, quindi fuori dalla canvas).
+	var l = eventi.length;
+		while ((tempoFinale - eventi[l=l-1].split(" ")[0])/velox > ctx.canvas.width) eventi.pop()
 
+// pulizia e successivo ridisegno della canvas.
 	ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
 	cty.clearRect(0,0,cty.canvas.width,cty.canvas.height);
 	ctz.clearRect(0,0,ctz.canvas.width,ctz.canvas.height);
-	ctx.beginPath(); 
-	cty.beginPath(); 
-	ctz.beginPath(); 
+
+	ctx.beginPath();
+	cty.beginPath();
+	ctz.beginPath();
 	for (var i=0;i<eventi.length;i++) {
 		var coord = eventi[i].split(" ");
 		if (coord.length==4) {
-   		ctx.lineTo((tempoFinale - coord[0])/50000,50+Number(coord[1]));
-   		cty.lineTo((tempoFinale - coord[0])/50000,50+Number(coord[2]));
-   		ctz.lineTo((tempoFinale - coord[0])/50000,-245+50+Number(coord[3]));
+   		ctx.lineTo((tempoFinale - coord[0])/velox,50+Number(coord[1]));
+   		cty.lineTo((tempoFinale - coord[0])/velox,50+Number(coord[2]));
+   		ctz.lineTo((tempoFinale - coord[0])/velox,-245+50+Number(coord[3]));
 		}
 	}
 	ctx.stroke();
 	cty.stroke();
 	ctz.stroke();
-	ajaxGet("http://localhost:8000",graph,"footer",tempoFinale);
-}
 
+// acquisizione ricorsiva di nuovi dati
+	ajaxGet("http://localhost:8000",graph,tempoFinale,velox);
+}
