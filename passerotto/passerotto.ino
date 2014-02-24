77f4c3160fac965adba5ec23abb62db7bfb510b0
cloc3@s939 <ziapannocchia@gmail.com>
2014-02-24 13:18:47 +0100

    ottimizzazione dello sketch passerotto

--- passerotto/passerotto.ino
+++ passerotto/passerotto.ino
@@ -1,44 +1,63 @@
 /*
-	skietch per una fotoresistenza e un led.	
+	sketch per una fotoresistenza e un led.	
 	Quando la fotoresistenza viene oscurata, il led pulsa ad alta frequenza,
 	come il cuore di un passerotto in un pugno.
 */
 
-#define LED 13                // LED collegato al pin digitale 13
-#define fotoResistenza 0
-#define ciclo 800
+#define LED 13								// LED collegato al pin digitale 13
+#define fotoResistenza 0			// indirizzo fisico del canale di lettura
+#define ciclo 800							// intervallo di acquisizione del dato
+#define MAXINPUT 850					// valore del canale di input in condizioni di massima illuminazione (minima resistenza)
+#define MININPUT 60						// valore del canale di input in condizioni di minima illuminazione (massima resistenza)
+#define MAXFREQ 25						// massima frequenza di lampeggio
+#define MINFREQ 1							// minima frequenza di lampeggio
+#define intensita 40					// tempo di accensione della lampadina (fissare inferiore a ciclo/MAXFREQ)
 
 int luce;
 int freq;
-int buio;
-int step;
-int i;
+int unoSuM;
+int lampeggio;
+
+void pulsazione(int lampeggiamento) {
+	digitalWrite(LED, HIGH);
+	delay(intensita);
+	digitalWrite(LED, LOW);
+	delay(lampeggiamento - intensita);
+}
 
 void setup()
 {
 	pinMode(LED, OUTPUT);       // imposta il pin digitale come output
 	Serial.begin(115200);
+	unoSuM=(MAXINPUT - MININPUT)/(MAXFREQ-MINFREQ); // reciproco del coefficiente angolare di conversione tra intervallo di input e intervallo di frequenza
 }
 
 void loop()
 {
-		//delay(5);
-		luce = analogRead(fotoResistenza);
-		Serial.println(luce);
-		buio = 950 - luce;
-		freq = 1 + buio / 30;
-		step = ciclo / freq;
-		if ( buio <= 0) {
-			digitalWrite(LED, HIGH);
-			delay(5);
-			digitalWrite(LED,LOW);
-			delay(ciclo -5);
-		} else {
-			for (i = 0;i <= freq; i++) {
-				digitalWrite(LED, HIGH);
-				delay(5);
-				digitalWrite(LED, LOW);
-				delay(step - 5);
-			}
-		}
+	int indice;
+
+	// lettura analogica
+	luce = analogRead(fotoResistenza);
+	freq = MAXFREQ - (luce - MININPUT)/unoSuM;
+
+	// stampa di controllo via seriale
+	Serial.print("luce = ");
+	Serial.println(luce);
+	Serial.print("frequenza = ");
+	Serial.println(freq);
+
+	// definizione della frequenza di lampeggiamento
+	lampeggio = ciclo / freq;
+	if ( freq < MINFREQ) {							// filtro per alta illuminazione
+		lampeggio=ciclo; 
+	} else if (freq > MAXFREQ ) {				// filtro per bassa illuminazione
+		lampeggio=ciclo/MAXFREQ; 
+	} else {
+		lampeggio=ciclo/freq; 
+	}
+
+	// esecuzione del lampeggiamento
+	for (indice = 0;indice < freq; indice++) {
+		pulsazione(lampeggio);
+	}
 }
